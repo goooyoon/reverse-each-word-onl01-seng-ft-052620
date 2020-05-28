0440d3c8bdfe36f52a335674891c90daa65c366b
Grace Kim <goooyoon@gmail.com>
2020-05-28 06:52:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,6 @@
 def reverse_each_word(sentence1)
-  
+  sentence1.reverse
 end
   
 
-
-
-	.	def hamburger(toppings)
-	.	  my_statements = []
-	.	  toppings.each do |topping|
-	.	    my_statements << "I love #{topping} on my burgers!"
-	.	  end
-	.	  my_statements
 	.	end
